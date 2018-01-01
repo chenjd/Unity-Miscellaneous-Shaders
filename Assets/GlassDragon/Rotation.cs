ae1cb4e13a31c1c7f4b20afdbae87b063abf7cc0
jiadong chen <609828261@qq.com>
2018-01-01 14:03:08 +0800

    add glass dragon

---
+++ Assets/GlassDragon/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (this.transform.position, Vector3.up, Time.deltaTime * 10);
+	}
+}
